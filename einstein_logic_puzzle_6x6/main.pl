3ebac07971b7855257f8314645effcfa5ffc0ec7
Tejks <50965095+tejks@users.noreply.github.com>
2022-11-07 03:45:02 +0100

    Update main.pl

--- einstein_logic_puzzle_6x6/main.pl
+++ einstein_logic_puzzle_6x6/main.pl
@@ -3,7 +3,7 @@ left(L,R,[_|O]) :- left(L,R,O).
 
 enter_data(X) :-
 	member([_,andrzej,agnieszka,_,_,_,_],X),
-    member([_,_,ola,_,czerwony,_,_],X),
+        member([_,_,ola,_,czerwony,_,_],X),
 	member([_,_,_,fiat,zielony,_,_],X),
 	member([_,_,_,opel,_,restauracja,_],X),
 	member([_,_,_,_,_,politechnika,czytanie],X),
@@ -17,22 +17,22 @@ enter_data(X) :-
     
 	left([_,_,_,fiat,_,_,_],[_,_,_,_,_,bank,_],X),
 	left([_,_,_,_,_,bank,_],[_,_,_,_,_,_,rowery],X),
-    left([_,_,_,_,granatowy,_,_],[_,_,_,_,_,hotel,_],X),
+        left([_,_,_,_,granatowy,_,_],[_,_,_,_,_,hotel,_],X),
 	left([_,_,_,_,_,hotel,_],[_,_,_,_,_,szpital,_],X),
-    left([_,_,ania,_,_,_,_],[_,michal,_,_,_,_,_],X),
+        left([_,_,ania,_,_,_,_],[_,michal,_,_,_,_,_],X),
 	left([_,mateusz,_,_,_,_,_],[_,_,ania,_,_,_,_],X),
-    left([_,_,_,_,zolty,_,_],[_,_,alicja,_,_,_,_],X),
+        left([_,_,_,_,zolty,_,_],[_,_,alicja,_,_,_,_],X),
 	left([_,_,_,_,_,_,majsterkowanie],[_,_,_,_,zolty,_,_],X).
 
 get_solution_1(X, S) :-
-    member([_,S,_,_,_,szpital,_],X).
+    	member([_,S,_,_,_,szpital,_],X).
 
 get_solution_2(X, S) :-
-    member([_,_,_,renault,S,_,_],X).
+    	member([_,_,_,renault,S,_,_],X).
 
 search(S1, S2) :-
-    Data = [[1,_,_,_,_,_,_],[2,_,_,_,_,_,_],[3,_,_,_,_,_,_],[4,_,_,_,_,_,_],[5,_,_,_,_,_,_],[6,_,_,_,_,_,_]],
-    enter_data(Data),
+    	Data = [[1,_,_,_,_,_,_],[2,_,_,_,_,_,_],[3,_,_,_,_,_,_],[4,_,_,_,_,_,_],[5,_,_,_,_,_,_],[6,_,_,_,_,_,_]],
+    	enter_data(Data),
     get_solution_1(Data, S1),
     get_solution_2(Data, S2).
     
